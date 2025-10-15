695f91db92536e5d14896228f1dda7bf935d7e4e
Syed Arham <arhamsyed161@gmail.com>
2025-10-15 16:52:16 +0600

    changes

--- Syed Arham/class2.py
+++ Syed Arham/class2.py
@@ -1,7 +1,17 @@
+# Write
+
 # f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/text.txt", "w")
 # f.write("Hello World")
 # f.close()
 
-f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/text.txt", "a")
+# Append
+
+# f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/text.txt", "a")
+# f.write("\nHello World")
+# f.close()
+
+# Create File
+
+f = open("hello4.txt", "w")
 f.write("\nHello World")
 f.close()
