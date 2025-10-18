bd91c7d6e97640dc4487a9f80ab86317db032294
Syed Arham <arhamsyed161@gmail.com>
2025-10-18 17:05:31 +0600

    Added class 3

---
+++ Syed Arham/class3.py
@@ -0,0 +1,10 @@
+f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/class3.txt", "r")
+print(f.read())
+print("#################DIVIDER#####################")
+
+f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/class3.txt", "a")
+f.write("\nThen will be following a proper stack")
+
+print("#################DIVIDER#####################")
+f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/class3.txt", "r")
+print(f.read())
