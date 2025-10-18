fbb2cc696f715717057c3c9434f86d5da004baeb
Syed Arham <arhamsyed161@gmail.com>
2025-10-18 17:20:52 +0600

    Class 3 end

--- Syed Arham/class3.py
+++ Syed Arham/class3.py
@@ -1,10 +1,21 @@
-f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/class3.txt", "r")
-print(f.read())
-print("#################DIVIDER#####################")
-
+f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/class3.txt", "wt")
+f.write("My name is Arham")
 f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/class3.txt", "a")
-f.write("\nThen will be following a proper stack")
+f.write("\nI am learning Python")
+f.write("\nThen I will learn Web Development")
 
-print("#################DIVIDER#####################")
 f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/class3.txt", "r")
-print(f.read())
+# print(f.read())
+
+# f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/class3.txt", "a")
+# f.write("\nThen will be following a proper stack")
+
+# print("#################DIVIDER#####################")
+
+# f = open("/home/arham/Codes/Python/NeuroFlex-Edu/Syed Arham/class3.txt", "r")
+# print(f.read())
+
+# f.close()
+
+print(f.readline())
+print(f.readline())
